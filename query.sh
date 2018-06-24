4f8c7a8bf5449e0bce7e92df8c722cda661aab2d
Vladdy <ivanov.vladdy@gmail.com>
2018-06-24 13:24:40 +0300

    Pcxchg with producerApp and marketApp, before event hub

---
+++ query.sh
@@ -0,0 +1,3 @@
+docker exec cli.Amazon bash -c "peer chaincode query -C asus -n pcxchg -c '{\"Args\":[\"queryStock\"]}'"
+docker exec cli.Amazon bash -c "peer chaincode query -C hp -n pcxchg -c '{\"Args\":[\"queryStock\"]}'"
+docker exec cli.Amazon bash -c "peer chaincode query -C dell -n pcxchg -c '{\"Args\":[\"queryStock\"]}'"
