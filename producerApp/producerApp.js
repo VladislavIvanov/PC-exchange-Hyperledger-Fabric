d870cd08948b1d247f538da8be08ac6ddf24a1f8
Vladdy <ivanov.vladdy@gmail.com>
2018-07-31 15:48:26 +0300

    Producer app worked with copied certificate not generate from FA

--- producerApp/producerApp.js
+++ producerApp/producerApp.js
@@ -2,35 +2,7 @@
 
 // Functions from figure
 const hfc = require('fabric-client');
-
-const target = [];
-const client = new hfc();
 let channel;
-
-const ca = require('fabric-ca-client');
-const User = require('fabric-client/lib/User.js');
-
-const enrolUserCA = function(client, opt) {
-    // Create the ca client
-    const caClient = new ca(opt.ca_url, null, '');
-
-    return hfc.newDefaultKeyValueStore({ path: opt.wallet_path })
-        .then(wallet => {
-            client.setStateStore(wallet);
-            return caClient.enroll({
-                enrollmentID: "admin",
-                enrollmentSecret: "adminpw"
-            });
-        })
-        .then(enrollment => {
-            const admin = new User("admin");
-            admin.setCryptoSuite(client.getCryptoSuite());
-            return admin.setEnrollment(enrollment.key, enrollment.certificate, opt.msp)
-                .then(() => client.setUserContext(admin))
-                .then(() => admin);
-        });
-};
-
 const enrolUser = function(client, options) {
     return hfc.newDefaultKeyValueStore({ path: options.wallet_path })
         .then(wallet => {
@@ -44,11 +16,11 @@ const initNetwork = function(client, options, target) {
     try {
         channel = client.newChannel(options.channel_id);
         const peer = client.newPeer(options.peer_url);
-        target = peer;
+        target.push(peer);
         channel.addPeer(peer);
         channel.addOrderer(client.newOrderer(options.orderer_url));
     } catch(e) { // channel already exists
-        channel= client.getChannel(options.channel_id);
+        channel = client.getChannel(options.channel_id);
     }
     return channel;
 };
@@ -75,40 +47,12 @@ const sendOrderer = function(channel, request) {
     return channel.sendTransaction(request);
 };
 
-const initEventHub = function(client, eventUrl) {
-    const eh = client.newEventHub();
-    eh.setPeerAddr(eventUrl);
-    eh.connect();
-    return eh;
-};
-
-const catchEvent = function(eh, transactionID, timeout) {
-    return new Promise((resolve, reject) => {
-        const handle = setTimeout(
-            () => {
-                eh.unregisterTxEvent(transactionID);
-                eh.disconnect();
-                reject("Timed out");
-            },
-            timeout);
-
-        const txId = transactionID.getTransactionID();
-        eh.registerTxEvent(txId, (tx, code) => {
-            clearTimeout(handle);
-            eh.unregisterTxEvent(transactionID);
-            eh.disconnect();
-
-            if (code == 'VALID')
-                return resolve("Transaction is in a block.");
-            reject("Transaction is rejected. Code: " + code.toString());
-        });
-
-    });
-};
+const target = [];
+const client = new hfc();
 
 // Function invokes createPC on pcxchg
 function invoke(opt, param) {
-    return enrolUserCA(client, opt)
+    return enrolUser(client, opt)
         .then(user => {
             if(typeof user === "undefined" || !user.isEnrolled())
                 throw "User not enrolled";
@@ -122,25 +66,19 @@ function invoke(opt, param) {
                 chainId: opt.channel_id,
                 txId: null
             };
-            return transactionProposal(client, channel, request)
-                .then(results => {
-                    if (responseInspect(results)) {
-                        const broadcastRequest = {
-                            proposalResponses: results[0],
-                            proposal: results[1],
-                            header: results[2]
-                        };
-
-                        let eh = initEventHub(client, opt.event_url);
-
-                        return Promise.all([
-                            sendOrderer(channel, broadcastRequest),
-                            catchEvent(eh, request.txId, 6000)
-                        ]);
-                    } else {
-                        throw "Bad Response";
-                    }
-                });
+            return transactionProposal(client, channel, request);
+        })
+        .then(results => {
+            if (responseInspect(results)) {
+                const request = {
+                    proposalResponses: results[0],
+                    proposal: results[1],
+                    header: results[2]
+                };
+                return sendOrderer(channel, request);
+            } else {
+                throw "Response is bad";
+            }
         })
         .catch(err => {
             console.log(err);
@@ -148,41 +86,34 @@ function invoke(opt, param) {
         });
 };
 
-var options = {
+// Options
+const options = {
     Asus : {
-        wallet_path: '/Users/vladislavivanov/HLF/pcxchg/producerApp/certs/asus',
+        wallet_path: '/Users/vladislavivanov/HLF/pcxchg/producerApp/certs',
         user_id: 'AsusAdmin',
         channel_id: 'asus',
         chaincode_id: 'pcxchg',
         peer_url: 'grpc://localhost:7051',
-        orderer_url: 'grpc://localhost:7050',
-        event_url: 'grpc://localhost:7053',
-        ca_url: 'http://localhost:8054',
-        msp: `AsusMSP`
+        orderer_url: 'grpc://localhost:7050'
     },
-    Hp : {
-        wallet_path: '/Users/vladislavivanov/HLF/pcxchg/producerApp/certs/hp',
-        user_id: 'HpAdmin',
+    HP : {
+        wallet_path: '/Users/vladislavivanov/HLF/pcxchg/producerApp/certs',
+        user_id: 'HPAdmin',
         channel_id: 'hp',
         chaincode_id: 'pcxchg',
         peer_url: 'grpc://localhost:9051',
-        orderer_url: 'grpc://localhost:7050',
-        event_url: 'grpc://localhost:9053',
-        ca_url: 'http://localhost:9054',
-        msp: `HPMSP`
+        orderer_url: 'grpc://localhost:7050'
     },
     Dell : {
-        wallet_path: '/Users/vladislavivanov/HLF/pcxchg/producerApp/certs/dell',
+        wallet_path: '/Users/vladislavivanov/HLF/pcxchg/producerApp/certs',
         user_id: 'DellAdmin',
         channel_id: 'dell',
         chaincode_id: 'pcxchg',
         peer_url: 'grpc://localhost:10051',
-        orderer_url: 'grpc://localhost:7050',
-        event_url: 'grpc://localhost:10053',
-        ca_url: 'http://localhost:10054',
-        msp: `DellMSP`
+        orderer_url: 'grpc://localhost:7050'
     }
 };
+
 
 // Server
 const express = require("express");
@@ -202,7 +133,7 @@ app.set('views', __dirname);
 app.post('/invoke', function(req, res, next) {
     const args = req.body.args;
     invoke(options[args[0]], args.slice(1))
-        .then(result => res.send(result))
+        .then(() => res.send("Chaincode invoked"))
         .catch(err => {
             res.status(500);
             res.send(err.toString());
